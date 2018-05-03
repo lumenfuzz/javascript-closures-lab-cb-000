5ecda186f8771d12c994be4afd82d1961c9a788a
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-03 10:53:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    counter = counter + 1
+  }
+  function getBumps() {
+    return counter
+  }
+  return { addBump, getBumps }
+}
